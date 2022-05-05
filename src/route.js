4f2dccdfc11fcc7cc4fd34c8febe6fff4a8d380d
Alinne Lauren <alinnelaurensz@gmail.com>
2022-05-05 01:16:40 -0300

    Git ignore

--- src/route.js
+++ src/route.js
@@ -4,6 +4,7 @@ const RoomController = require('./controllers/RoomController')
 
 const route = express.Router()
 
+
 route.get('/', (req, res) => res.render('index', { page: 'enter-room' }))
 route.get('/create-pass', (req, res) =>  res.render('index', { page: 'create-pass' }))
 
